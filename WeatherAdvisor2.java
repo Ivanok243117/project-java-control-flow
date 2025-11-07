cd70c20f6b40619d122ff1ab63c4cdd0d2db194e
Ivanok <ivan243117@gmail.com>
2025-11-06 19:59:58 -0500

    Actualiza WeatherAdvisor2.java FizzBuzzBoom  para múltiplos de 3, 5 y 7

--- WeatherAdvisor2.java
+++ WeatherAdvisor2.java
@@ -1,16 +1,24 @@
-
+import java.util.Scanner;
 public class WeatherAdvisor2 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digita un numero para el Rango a usar para nuestro juego FizzBuzzBoom: ");
+        int Rango = scanner.nextInt();
+
         
-        for (int i= 1; i<=100; i++) {
+        for (int i= 1; i<= Rango; i++) {
         
-        if(i % 3 ==0 && i % 5 ==0) {
-            System.out.println("FizzBuzz");
+        if(i % 3 ==0 && i % 5 ==0 && i % 7 ==0) {
+            System.out.println("FizzBuzzBoom");
         } else if (i % 3 ==0) {
             System.out.println("Fizz");
         } else if (i % 5 ==0) {
             System.out.println("Buzz");
-        } else {
+        } else if (i % 7 ==0) {
+            System.out.println("Boom");
+        }else {
             System.out.println(i);
         }
         
